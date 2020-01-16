4d7bec98537bae8b8f8cefa135105ae1851021cf
Olexander <kvazis@gmail.com>
2020-01-16 12:58:02 +0200

    Fixed exports (#30)
    * feat: connector (#25)
    
    * fix: fixed several policies, which terminate flow (#26)
    
    * fix: beautifised imports (#27)
    
    * fix: removed lock.json
    
    * fix: fixed exports
    
    Co-authored-by: Gérard TOKO <gerardtoko@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,11 @@ export { default as Request } from "./Request";
 export { default as Response } from "./Response";
 export { default as Context } from "./Context";
 export { default as Scope } from "./policies/Scope";
-export { default as ExecutionContext } from "./policies/executioncontext";
+export { default as ExecutionContext } from "./policies/ExecutionContext";
 export { default as PolicyCategory } from "./policies/PolicyCategory";
 export { default as YapPolicy } from "./policies/decorator";
-
 export { default as Policy } from './policies/policy';
+
 export { default as Connector } from './connectors/connector';
 export { default as Connection } from './connectors/connection';
 export { default as YapConnector } from './connectors/decorator';
